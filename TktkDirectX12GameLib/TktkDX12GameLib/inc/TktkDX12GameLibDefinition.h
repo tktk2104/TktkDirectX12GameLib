468044f1251d6b81c51d8670b0ebc07abe25e40b
DESKTOP-K2TRCD7\takan <taka.nihonkogakuin.2104@gmail.com>
2020-08-25 02:08:21 +0900

    [ver 1.01.02] Update TktkLib LibDefinition

--- TktkDirectX12GameLib/TktkDX12GameLib/inc/TktkDX12GameLibDefinition.h
+++ TktkDirectX12GameLib/TktkDX12GameLib/inc/TktkDX12GameLibDefinition.h
@@ -1,14 +1,24 @@
 #ifndef TKTK_DX12_GAME_LIB_DEFINITION_H_
 #define TKTK_DX12_GAME_LIB_DEFINITION_H_
 
+#pragma comment(lib, "d3d12.lib" )
+#pragma comment(lib, "dxgi.lib" )
 #pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "xaudio2.lib")
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
 
 #if defined(NDEBUG) || !defined(_DEBUG)
+#pragma comment( lib, "TktkMathLib_mt.lib" )
+#pragma comment( lib, "TktkContainerLib_mt.lib" )
+#pragma comment( lib, "TktkFileIoLib_mt.lib" )
+#pragma comment( lib, "TktkDX12WrappingLib_mt.lib" )
 #pragma comment( lib, "TktkDX12GameLib_mt.lib" )
 #else
+#pragma comment( lib, "TktkMathLib_mtd.lib" )
+#pragma comment( lib, "TktkContainerLib_mtd.lib" )
+#pragma comment( lib, "TktkFileIoLib_mtd.lib" )
+#pragma comment( lib, "TktkDX12WrappingLib_mtd.lib" )
 #pragma comment( lib, "TktkDX12GameLib_mtd.lib" )
 #endif
 #endif // !TKTK_DX12_GAME_LIB_DEFINITION_H_
