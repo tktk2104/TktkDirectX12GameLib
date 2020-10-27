4ff236528feb3aaa71686b9489c0ad843d5d4a24
LAPTOP-U0HE3CFQ\G017A1380 <taka.lalpedhuez@2104.gmail.com>
2020-10-28 00:29:10 +0900

    [ver 1.01.69] Add TktkDXGameLib IncludeFile

---
+++ TktkDirectX12GameLib/TktkDX12GameLib/inc/TktkDX12GameLib.h
@@ -0,0 +1,6 @@
+#ifndef TKTK_DX12_GAME_LIB_H_
+#define TKTK_DX12_GAME_LIB_H_
+
+#include "TktkDX12Game/TktkDX12Game.h"
+#include "TktkDX12BaseComponents/Components.h"
+#endif // !TKTK_DX12_GAME_LIB_H_
